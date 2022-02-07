0c52e75db3e95573aed452eaade7c3f90a2c194b
tecube <tecube3@gmail.com>
2022-02-07 18:15:10 +0900

    beta

--- gen_wave_data.py
+++ gen_wave_data.py
@@ -9,4 +9,22 @@ Nt = 1000
 xs, dx = np.linspace(0, L, Nx, retstep=True)
 ts, dt = np.linspace(0, T, Nt, retstep=True)
 
-u = np.zeros([Nt, Nstrings, Nx])
+u = np.zeros([Nstrings, Nx])
+
+for i, t in enumerate(ts):
+    print(i)
+    for s in range(Nstrings):
+        u[s,:] = np.sin(xs*s*0.1 + t)
+
+    np.savetxt(f'wave_data/u_{i:03}.dat', u)
+
+#u = np.zeros([Nt, Nstrings, Nx])
+
+#for i, t in enumerate(ts):
+    #print(i)
+    #for s in range(Nstrings):
+        #u[i,s,:] = np.cos(i) * np.sin(xs*s)
+
+#with open('wave_data/all.dat', 'a') as f:
+    #for t in ts:
+        #np.savetxt(f, u, header=f'{t}')
--- surfing.py
+++ surfing.py
@@ -1,42 +1,85 @@
+import glob
+from os import path
 import numpy as np
 import matplotlib.pyplot as plt
 import PySimpleGUI as sg
+from tqdm import tqdm
 
 
 def make_empty_plot():
     fig = plt.figure()
 
-    ax = fig.add_subplot()
-
-    x = np.arange(0, 2*np.pi, 0.05*np.pi)
+    ax = fig.add_subplot(projection='3d')
 
     plt.show(block=False)
 
-    return fig, ax, x
+    return ax
 
 
-def update_plot(offset, fig, ax, x):
+def load_data(dirname):
+    files = glob.glob(path.normpath(f'{dirname}/*.dat'))
+
+    if len(files) == 0:
+        print('file not found')
+        return
+    
+    common = path.commonprefix(files)
+    print(f'common filename: {common}')
+
+    frames = [None] * len(files)
+    print(f'{len(files)} frames found')
+
+    for f in tqdm(files):
+        frame_num = int(f[len(common):-4]) # -4: len('.dat'))
+        frames[frame_num] = np.loadtxt(f)
+    
+    return frames
+
+
+def make_grid(frame):
+    assert frame.ndim == 2
+
+    #### *.dat ####
+    #  x →
+    # y
+    # ↓
+    x = np.arange(frame.shape[1])
+    y = np.arange(frame.shape[0])
+
+    return np.meshgrid(x, y)
+
+
+def update_plot(X, Y, Z, ax):
     ax.clear()
-    ax.plot(x+offset, np.sin(x+offset))
+    ax.plot_surface(X, Y, Z)
     plt.show(block=False)
 
 
 def main():
     layout = [
-        [sg.Slider(range=(0,100), default_value=0, resolution=1, orientation='horizontal', enable_events=True, key='-time_slider-')],
+        [sg.Button('Data Dir')],
+        [sg.Slider(range=(0,1), default_value=0, resolution=1, orientation='horizontal', enable_events=True, key='time_slider', disabled=True)],
     ]
 
     window = sg.Window('surfing', layout, finalize=True)
 
-    fig, ax, x = make_data_fig()
+    ax = make_empty_plot()
+
     while True:
         event, values = window.read()
 
         if event in (None, 'Cancel'):
             break
+        
+        elif event == 'Data Dir':
+            dirname = sg.popup_get_folder('Data directory')
+            frames = load_data(dirname)
+            X, Y = make_grid(frames[0])
+            window['time_slider'].update(range=(0, len(frames)-1), disabled=False)
+            update_plot(X, Y, frames[0], ax)
 
-        elif event == '-time_slider-':
-            draw_plot(values['-time_slider-'], fig, ax, x)
+        elif event == 'time_slider':
+            update_plot(X, Y, frames[int(values['time_slider'])], ax)
 
     window.close()
 
